5bd74d4fbd612b45281be48f7ea41b1ca270ddad
Ben Siraphob <bensiraphob@gmail.com>
2026-08-12 10:18:30 -0700

    Add reproducible cmrustc development shell

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "mrustc and cmrustc development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          linuxDebugTools = pkgs.lib.optionals pkgs.stdenv.isLinux [
+            pkgs.gdb
+            pkgs.ltrace
+            pkgs.rr
+            pkgs.strace
+            pkgs.valgrind
+          ];
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              bashInteractive
+              clang
+              file
+              gcc
+              gh
+              git
+              gnumake
+              jq
+              perl
+              pkg-config
+              python3
+              ripgrep
+              tinycc
+            ] ++ linuxDebugTools;
+
+            shellHook = ''
+              export CMRUSTC_ROOT="$PWD/cmrustc"
+            '';
+          };
+        });
+    };
+}
